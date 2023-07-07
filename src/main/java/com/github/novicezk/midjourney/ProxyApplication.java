8ffe8f50fcb536c1e1ca3510be65e406b38a4833
Vaibhav Chotaliya <vrc29797@gmail.com>
2023-07-07 22:19:09 +0530

    updated Cors

--- src/main/java/com/github/novicezk/midjourney/ProxyApplication.java
+++ src/main/java/com/github/novicezk/midjourney/ProxyApplication.java
@@ -2,34 +2,39 @@ package com.github.novicezk.midjourney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import spring.config.BeanConfig;
 import spring.config.WebMvcConfig;
+
+import java.util.Collections;
 
 @EnableScheduling
 @SpringBootApplication
 @Import({BeanConfig.class, WebMvcConfig.class})
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*")
-						.allowedOrigins("*")
-						.allowedHeaders("*")
-						.allowedMethods("*")
-						.allowedOriginPatterns("*");
-			}
-		};
-	}
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 }
